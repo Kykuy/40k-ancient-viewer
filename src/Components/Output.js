aa82470131d02d2d10543ac0c4de68a23d3ad6ca
Kykuy <illidane2@gmail.com>
2021-08-02 17:06:39 +0300

    Applied initial styling to output

--- src/Components/Output.js
+++ src/Components/Output.js
@@ -1,20 +1,22 @@
+import './Output.css'
+
 function Output(props) {
   const { enchants, unlockReqs, selectedItemType, selectedAffix } = props;
 
   return (
-    <>
+    <article className = 'itemCard'>
       <p className = 'itemName'>{selectedItemType.label} of {selectedAffix.value.slice(0, selectedAffix.value.indexOf('_'))}</p>
-      <p className = 'ancientEnch'
+      <p className = 'ancientEnch enchantDescription'
       data-tooltip = {`${unlockReqs[selectedAffix.value].ancientEnch.condition}(${unlockReqs[selectedAffix.value].ancientEnch.value})`}
-      >Ancient Relic enchant: {enchants[selectedAffix.value].ancientEnch}</p>
+      ><span className = 'enchTypeIndicator'></span><span className = 'enchText'>{enchants[selectedAffix.value].ancientEnch}</span></p>
       
       {[1, 2, 3, 4].map( number => { // there are 4 base enchants, create a description for each
         // dangerouslySetInnerHTML may theoretically expose us to XSS attack, but since we don't store any sensitive data we just don't care
-        return <p key = {`enchant#${number}`} className = 'primaryEnch' 
-        dangerouslySetInnerHTML = {{__html: `Regular enchant №${number}: ${enchants[selectedAffix.value][`ench${number}`]}`}}
+        return <p key = {`enchant#${number}`} className = 'primaryEnch enchantDescription' 
+        dangerouslySetInnerHTML = {{__html: `<span class = 'enchTypeIndicator'></span><span class = 'enchText'>${enchants[selectedAffix.value][`ench${number}`]}</span>`}}
         data-tooltip = {`${unlockReqs[selectedAffix.value][`ench${number}`].condition}(${unlockReqs[selectedAffix.value][`ench${number}`].value})`}></p> // outputs 'condition text(value)'
       })}
-    </>
+    </article>
   )
 
 }
